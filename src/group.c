02cc4dca18139a6942972c59c3582589ce1c255a
Kitana <todd@frogtechnical.com>
2012-10-24 22:34:17 +0000

    enabling goodie ap, new ap spec

--- src/group.c
+++ src/group.c
@@ -279,6 +279,7 @@ void do_group(P_char ch, char *argument, int cmd)
     send_to_char("You feel like being alone right now.\n", ch);
     return;
   }
+
   if (!*name)
   {
     if (!ch->group)
@@ -649,6 +650,47 @@ void do_group(P_char ch, char *argument, int cmd)
     act("$N doesn't want to be in your group.", TRUE, ch, 0, victim, TO_CHAR);
     return;
   } */
+
+//Paladins and AP cannot group together - Drannak
+if(GET_CLASS(victim, CLASS_PALADIN))
+{
+for (gl = ch->group; gl; gl = gl->next)
+ {
+  if (GET_CLASS(gl->ch, CLASS_ANTIPALADIN))
+   {
+    send_to_char("&+WYou do not wish to group with such unholy scum!&n\n", victim);
+    send_to_char("&+LAhh, but grouping with someone so &+Wholy &+Lwould disturb the unholy energies of your group.&n\n", ch);
+	return;
+   }
+  
+  }
+ }
+
+if(GET_CLASS(victim, CLASS_ANTIPALADIN))
+{
+for (gl = ch->group; gl; gl = gl->next)
+ {
+  if (GET_CLASS(gl->ch, CLASS_PALADIN))
+   {
+    send_to_char("&+WYou do not wish to group with such holy scum!&n\n", victim);
+    send_to_char("&+WAhh, but grouping with someone so &+Lunholy &+Wwould disturb the holy energies of your group.&n\n", ch);
+	return;
+   }
+  
+  }
+ }
+if(GET_CLASS(victim, CLASS_ANTIPALADIN) && GET_CLASS(ch, CLASS_PALADIN))
+{
+    send_to_char("&+WYou do not wish to group with such unholy scum!&n\n", ch);
+    send_to_char("&+LAhh, but grouping with someone so &+Wholy &+Lwould disturb the unholy energies of your group.&n\n", victim);
+	return;
+   }
+if(GET_CLASS(victim, CLASS_PALADIN) && GET_CLASS(ch, CLASS_ANTIPALADIN))
+  {
+    send_to_char("&+WYou do not wish to group with such holy scum!&n\n", ch);
+    send_to_char("&+WAhh, but grouping with someone so &+Lunholy &+Wwould disturb the holy energies of your group.&n\n", victim);
+	return;
+   }
 
   if (IS_NPC(victim) && (victim->following != ch) &&
       !is_linked_to(ch, victim, LNK_CONSENT))
