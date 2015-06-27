dd757df10458d0312615470184eda2ef00605666
Lohrr <lohrr@durismud.com>
2015-06-27 14:33:00 +0000

    Paladins/APs need to be using a 2h sword (or 2h longsword) in order to use their shieldless bash ability.

--- src/actoff.c
+++ src/actoff.c
@@ -5600,21 +5600,18 @@ if((GET_RACE(victim) == RACE_OGRE) && ch_size < vict_size)
 
   if( IS_HUMANOID(ch) )
   {
-    if( !(ch->equipment[WEAR_SHIELD]) && (GET_CLASS(ch, CLASS_PALADIN | CLASS_ANTIPALADIN))
-      && GET_CHAR_SKILL(ch, SKILL_SHIELDLESS_BASH) < 1 )
+    // Note: IS_PAL..SWORD checks to make sure weapon exists.
+    if( !(ch->equipment[WEAR_SHIELD]) && GET_CHAR_SKILL(ch, SKILL_SHIELDLESS_BASH) < 1
+      && GET_CLASS(ch, CLASS_PALADIN | CLASS_ANTIPALADIN) && IS_PALADIN_SWORD(weap) )
     {
-      if( weap )
+      // Artifacts are often low weight to allow all to use -  Jexni 6/7/08
+      if( IS_ARTIFACT(weap) )
       {
-        if( IS_ARTIFACT(weap) )
-        {
-          percent_chance += 7.0;
-           // Artifacts are often low weight to allow all
-           // to use -  Jexni 6/7/08
-        }
-        else
-        {
-          percent_chance += weap->weight / 2.0;
-        }
+        percent_chance += 7.0;
+      }
+      else
+      {
+        percent_chance += weap->weight / 2.0;
       }
     }
     else
