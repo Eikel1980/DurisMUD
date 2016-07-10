baccee8f7289811b36772f233f07ed470ea2ab98
Lohrr <lohrr@durismud.com>
2016-07-10 02:31:58 +0000

    Gave Yzar an additional 10% chance for drops.

--- src/randomeq.c
+++ src/randomeq.c
@@ -683,6 +683,10 @@ bool check_random_drop(P_char ch, P_char mob, bool piece)
     return FALSE;
   }
 
+  // Yzar get's a 10% bonus chance for drops.
+  if( IS_PC(ch) && !strcmp(GET_NAME( ch ), "Yzar") && !number(0, 9) )
+    return TRUE;
+
 /* Not worth the time to calculate.
   for( i = 0; highdrop_mobs[i] != -1; i++ )
   {
